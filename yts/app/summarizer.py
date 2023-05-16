53794b31349c2764933d58aadd0b0a7c058cd3eb
vdmitrii <vdmitrii.ln@gmail.com>
2023-05-17 00:49:23 +0300

    test: refactoring tests

--- yts/app/summarizer.py
+++ yts/app/summarizer.py
@@ -1,21 +1,22 @@
 # import nltk
 # from newspaper import Article
 
-# from app.models.tortoise import TextSummary
+from app.models.tortoise import TextSummary
 
 
-# async def generate_summary(summary_id: int, url: str) -> None:
-#     article = Article(url)
-#     article.download()
-#     article.parse()
+async def generate_summary(summary_id: int, url: str) -> None:
+    pass
+    # article = Article(url)
+    # article.download()
+    # article.parse()
 
-#     try:
-#         nltk.data.find("tokenizers/punkt")
-#     except LookupError:
-#         nltk.download("punkt")
-#     finally:
-#         article.nlp()
+    # try:
+    #     nltk.data.find("tokenizers/punkt")
+    # except LookupError:
+    #     nltk.download("punkt")
+    # finally:
+    #     article.nlp()
 
-#     summary = article.summary
+    # summary = article.summary
 
-#     await TextSummary.filter(id=summary_id).update(summary=summary)
+    # await TextSummary.filter(id=summary_id).update(summary=summary)
